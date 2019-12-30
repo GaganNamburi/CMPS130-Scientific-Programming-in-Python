6587abcff02476061f2368aaea86708e3a5be3d4
GaganNamburi <52763601+GaganNamburi@users.noreply.github.com>
2019-12-30 16:22:14 -0500

    Create Midterm5.py

---
+++ Midterm/Midterm5.py
@@ -0,0 +1,8 @@
+x = int(input("Please enter a positive number: "))
+y = int(input("Please enter a positive number: "))
+
+while(True):
+		if max % x == 0 and max % y == 0:
+			return multi
+			break
+		max += 1
